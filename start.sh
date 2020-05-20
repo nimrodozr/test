eff86a70e86a23bf99faa4a6d134830ea224ed51
nimrodozr <47597626+nimrodozr@users.noreply.github.com>
2020-05-20 14:43:33 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-java -javaagent:/opt/appdynamics/machine-agent/AppServerAgent-4.5.16.28759/javaagent.jar -Dappdynamics.agent.tierName=quarkus-handshake -Dappdynamics.agent.reuse.nodeName=true -Dappdynamics.agent.reuse.nodeName.prefix=quarkus-handshake
+exec ./application -Dquarkus.http.host=0.0.0.0
+exec java -javaagent:/opt/appdynamics/machine-agent/AppServerAgent-4.5.16.28759/javaagent.jar -Dappdynamics.agent.tierName=quarkus-handshake -Dappdynamics.agent.reuse.nodeName=true -Dappdynamics.agent.reuse.nodeName.prefix=quarkus-handshake
